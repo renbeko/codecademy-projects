c7282972aaefe36a1538debd304fd9ff39e631d3
serge <sergeisonkin@gmail.com>
2022-01-28 10:30:41 +0500

    Added Project #7

---
+++ 7-Secret-Message/main.js
@@ -0,0 +1,12 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+secretMessage.push('to', 'Program');
+
+const indexOfEasily = secretMessage.indexOf('easily');
+secretMessage[indexOfEasily] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 5, 'know,')
+
+console.log(secretMessage.join(' '));
